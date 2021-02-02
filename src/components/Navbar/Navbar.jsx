2343bc304fb80a0890a22aef52f05085b27e5dd0
Aryan Kumar <aryankumar1504@gmail.com>
2021-02-03 00:41:54 +0530

    Products, Product, Navbar Done(Item Add to cart feature Added)

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  /*MenuItem, Menu,*/ Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+// import classes from '*.module.css';
+
+import useStyles from "./styles";
+
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src="https://i.ibb.co/Qp1SXBw/commerce.png"
+              alt="MyShop"
+              height="25px"
+              className={classes.image}
+            />
+            MyShop
+          </Typography>
+          <div className={classes.grow} />
+          <div className="classes.button">
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
