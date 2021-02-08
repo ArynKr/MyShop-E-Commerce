bb59c1ff31c1f2f5b5086a406115ef4e64f924f2
Aryan Kumar <aryankumar1504@gmail.com>
2021-02-08 19:07:06 +0530

    Cart Functionality Added

--- src/components/Cart/CartItem/styles.js
+++ src/components/Cart/CartItem/styles.js
@@ -1,21 +1,19 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
-  root: {
-    // maxWidth: 345, original width style
-    maxWidth: "100%",
-  },
   media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
+    height: 260,
     backgroundSize: "contain",
-  },
-  cardActions: {
-    display: "flex",
-    justifyContent: "flex-end",
   },
   cardContent: {
     display: "flex",
     justifyContent: "space-between",
+  },
+  cartActions: {
+    justifyContent: "space-between",
+  },
+  buttons: {
+    display: "flex",
+    alignItems: "center",
   },
 }));
